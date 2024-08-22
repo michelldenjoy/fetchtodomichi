59551c2c07ddec07429d411996e3c5e3c8c6f3ef
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-08-22 19:31:44 +0200

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,16 @@
-import React from "react";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TodoList from './TodoList'
+import './index.css'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+ReactDOM.createRoot(document.getElementById('root')).render(
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+  <React.StrictMode>
+
+    <div className='vh-100 d-flex align-items-center'>
+      <TodoList />
+    </div>
+
+  </React.StrictMode>,
+)
